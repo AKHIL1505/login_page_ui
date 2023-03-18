c78820f9b44bfc25e0be44869c2f5f0168509f24
AKHIL <akhil.1505.ak@gmail.com>
2023-03-18 16:54:37 +0530

    COMMIT

--- lib/Login Page/login_page_as.dart
+++ lib/Login Page/login_page_as.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:sampleprojects/signup_page.dart';
+import 'package:sampleprojects/Login%20Page/signup_page.dart';
 
 import 'home_screen.dart';
 
